80b60774819bca96ac292cdd04e60f9f2de6d4dd
thecatalystai <23524186+thecatalystai@users.noreply.github.com>
2026-04-08 14:33:08 +0300

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HH7SV7Q68Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HH7SV7Q68Y');
+</script>
